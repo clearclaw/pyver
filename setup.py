e776a809bc6bd5479e46fe0a6bcce869fae97616
J C Lawrence <jcl@matterport.com>
2014-09-15 16:48:24 -0700

    Download URL for PyPi

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup (
     author = "J C Lawrence",
     author_email = "claw@kanga.nu",
     url = "https://github.com/clearclaw/pyver",
+    download_url = ("https://github.com/clearclaw/pyver/tarball/%s.%s"
+                    % (__version_info__[0], __version_info__[1])),
     license = "GPL v3",
     packages = find_packages (exclude = ["tests",]),
     package_data = {
