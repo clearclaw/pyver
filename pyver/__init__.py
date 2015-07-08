07954c042ccbd18aac772e6a723f4e466bbb0c23
J C Lawrence <jcl@matterport.com>
2015-07-07 21:45:10 -0700

    Great cleanup

--- pyver/__init__.py
+++ pyver/__init__.py
@@ -1,6 +1,3 @@
 #! /usr/bin/env python
 
-import pyver.metaversion as metaversion
-from pyver.metaversion import get_version
-import pyver.version as version
-from pyver.version import __version__, __version_info__
+from pyver.version import __version__, __version_info__, get_version
