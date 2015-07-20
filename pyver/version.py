4be1a46c6c4ee7887a45c95194d7cb6dc7bcc123
J C Lawrence <claw@kanga.nu>
2015-07-19 23:20:00 -0700

    lint

--- pyver/version.py
+++ pyver/version.py
@@ -1,17 +1,16 @@
 #! /usr/bin/env python
 
-def pyver_install ():
+try:
+  import pyver # pylint: disable=W0611,W0612
+except ImportError:
+  import os, subprocess
   try:
-    import pyver # pylint: disable=W0611,W0612
-  except ImportError:
-    import os, subprocess
-    try:
-      environment = os.environ.copy()
-      cmd = "pip install pyver".split (" ")
-      subprocess.check_call (cmd, env = environment)
-    except subprocess.CalledProcessError:
-      import sys
-      print >> sys.stderr, "Problem installing 'pyver' dependency."
-      print >> sys.stderr, "Please install pyver manually."
-      sys.exit (1)
-    import pyver # pylint: disable=W0611
+    environment = os.environ.copy()
+    cmd = "pip install pyver".split (" ")
+    subprocess.check_call (cmd, env = environment)
+  except subprocess.CalledProcessError:
+    import sys
+    print >> sys.stderr, "Problem installing 'pyver' dependency."
+    print >> sys.stderr, "Please install pyver manually."
+    sys.exit (1)
+  import pyver # pylint: disable=W0611
