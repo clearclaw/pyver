2c35c3ab0bfcce4c21b656fac3cad352b1215b60
J C Lawrence <claw@kanga.nu>
2015-08-01 20:56:14 -0700

    Cleaned up version.py

--- pyver/version.py
+++ pyver/version.py
@@ -1,16 +1,8 @@
 #! /usr/bin/env python
 
 try:
-  import pyver # pylint: disable=W0611,W0612
+  import pyver
 except ImportError:
-  import os, subprocess
-  try:
-    environment = os.environ.copy()
-    cmd = "pip install pyver".split (" ")
-    subprocess.check_call (cmd, env = environment)
-  except subprocess.CalledProcessError:
-    import sys
-    print >> sys.stderr, "Problem installing 'pyver' dependency."
-    print >> sys.stderr, "Please install pyver manually."
-    sys.exit (1)
+  import pip
+  pip.main (['install', 'pyver'])
   import pyver # pylint: disable=W0611
